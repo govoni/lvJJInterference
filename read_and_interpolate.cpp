d1152661b3eddadd08cd6a84b7d79b3a7df68d5b
lbrianza <lucabrianza@gmail.com>
2014-05-14 18:24:47 +0200

    bug fixed

--- read_and_interpolate.cpp
+++ read_and_interpolate.cpp
@@ -17,6 +17,7 @@
 #include "TTree.h"
 #include "TFile.h"
 #include "TH1F.h"
+#include "TH2D.h"
 #include "THStack.h"
 #include "TString.h"
 #include "TLegend.h"
@@ -49,7 +50,7 @@ int main(int argc, char *argv[])
   TString parameters_normal [9] = {"Norm","Mean_CB","Sigma_CB","alphaR_CB","nR_CB","alphaL_CB","nL_CB","R","Tau"};
 
   double mass_chosen[5] = {600,700,800,900,1000};
-  double c_chosen[6] = {0.100001,0.3,0.500001,0.7,0.900001,0.999};
+  double c_chosen[6] = {0.1,0.3,0.5,0.7,0.9,1.0};
   int Npar=9;
 
   TString *readfile = new TString ("file_for_interpolation.root"); //file with the values of the all parameters
@@ -62,18 +63,36 @@ int main(int argc, char *argv[])
   TGraph2D *igraph_SI[9];
   TGraph2D *igraph_S[7];
 
+  TH2D *ihisto_SI[9];
+  TH2D *ihisto_S[7];
+
   for (int i=0; i<Npar; i++) {
 
 	TString *name = new TString (parameters_normal[i]);  
 	name->Append("_SI.txt");
+	TString *name2 = new TString (parameters_normal[i]);  
+	name2->Append("_SI_histo");
+
 	igraph_SI[i] = (TGraph2D*)SI->Get(name->Data());
+	//	ihisto_SI[i] = (TH2D*)SI->Get(name2->Data());
+	//ihisto_SI[i]->SetDirectory(0);
+	//	igraph_SI[i]->GetHistogram("empty");
+	igraph_SI[i]->SetMaxIter(500000);
   }
 
   for (int i=0; i<Npar-2; i++) {
 
 	TString *name = new TString (parameters_normal[i]);  
 	name->Append("_S.txt");
+	TString *name2 = new TString (parameters_normal[i]);  
+	name2->Append("_S_histo");
+
 	igraph_S[i] = (TGraph2D*)SI->Get(name->Data());
+	igraph_S[i]->SetMaxIter(500000);
+
+	//	ihisto_S[i] = (TH2D*)SI->Get(name2->Data());
+	//	ihisto_S[i]->SetDirectory(0);
+	//		igraph_S[i]->GetHistogram("empty");
    }
 
 
@@ -82,20 +101,30 @@ int main(int argc, char *argv[])
 
       for (int i=0; i<Npar; i++) {
 
-	cout<<m<<" "<<c<<" "<<i<<endl;
-	fill_param[i]=igraph_SI[i]->Interpolate(mass_chosen[m],c_chosen[c]);
+	//	cout<<m<<" "<<c<<" "<<i<<endl;
+	if (parameters_normal[i].Contains("Norm"))
+	    fill_param[i]=exp(igraph_SI[i]->Interpolate(mass_chosen[m],c_chosen[c]));
+	else
+	    fill_param[i]=igraph_SI[i]->Interpolate(mass_chosen[m],c_chosen[c]);
       }
       for (int i=0; i<Npar-2; i++) {
-	cout<<m<<" "<<c<<" "<<i+9<<endl;
-	fill_param[i+9]=igraph_S[i]->Interpolate(mass_chosen[m],c_chosen[c]);
+	//	cout<<m<<" "<<c<<" "<<i+9<<endl;
+	if (parameters_normal[i].Contains("Norm"))
+	    fill_param[i+9]=exp(igraph_S[i]->Interpolate(mass_chosen[m],c_chosen[c]));
+	else
+	    fill_param[i+9]=igraph_S[i]->Interpolate(mass_chosen[m],c_chosen[c]);
       }
-
-      //  for (int i=0; i<16; i++)   std::cout<<fill_param[i]<<std::endl;
+      // if (m==4 && c==5) {
+	//        for (int i=0; i<16; i++)   
+	//  std::cout<<fill_param[i]<<std::endl;
+      //      }
 
       TF1* f_ratio23 = new TF1 ("f_ratio23", ratio_crystalBallLowHighWithRise, 0, 2000, 16) ;
       f_ratio23->SetParameters (fill_param) ;
 
-      std::cout<<f_ratio23->Eval(mass_chosen[m])<<std::endl;
+      //    std::cout<<mass_chosen[m]<<" "<<c_chosen[c]<<" a 1000: "<<f_ratio23->Eval(1000)<<std::endl;
+
+
       grid<<mass_chosen[m]<<" "<<c_chosen[c]<<" "<<f_ratio23->Eval(mass_chosen[m])<<"\n";
     }
   }
